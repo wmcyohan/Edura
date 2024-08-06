71a9efa7a5317b253f63bcfe768e23bc737c5cdd
Charith Yohan <120025195+wmcyohan@users.noreply.github.com>
2024-08-06 15:36:13 +0530

    Initializing repo to continue

---
+++ src/Model/MySQL.java
@@ -0,0 +1,33 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MySQL {
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/edura", "root", "1234");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ResultSet execute(String query) throws Exception {
+
+        Statement statement = connection.createStatement();
+
+        if (query.startsWith("SELECT")) {
+            return statement.executeQuery(query);        
+        } else {
+            statement.executeUpdate(query);
+            return null;
+        }
+    }
+
+}
---
+++ src/Model/UserBean.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+
+public class UserBean {
+    private String fname;
+    private String lname;
+    private String nic;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+}
